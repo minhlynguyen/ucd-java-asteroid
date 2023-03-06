3e2ef9837c2969a15262650cb8196420365a14b4
Cian Belton <cianbelton@protonmail.com>
2023-03-06 16:14:54 +0000

    merging code

---
+++ demo/src/main/java/asteroid_app/initial/Character.java
@@ -0,0 +1,20 @@
+package asteroid_app.initial;
+
+import java.util.Random;
+
+public class Character {
+	
+	public static double initX;
+	public static double initY;
+	
+	public void Position(double initX, double initY){
+		
+//		Random rnd = new Random();
+//		initX = rnd.nextDouble(200);
+//		initY = rnd.nextDouble(200);
+//		
+//		this.initX = initX;
+//		this.initY = initY;
+		
+	}
+}

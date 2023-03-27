d906335c21454560eb0c958cf20947375ca1234b
Yuanjun Du <118921121+CodePie1@users.noreply.github.com>
2023-03-27 10:57:41 +0100

    Delete demo directory

--- demo/src/main/java/asteroid_app/initial/user_ship.java
+++
@@ -1,9 +0,0 @@
-package asteroid_app.initial;
-//import polygon to draw a polygon
-import javafx.scene.shape.Polygon;
-
-public class user_ship{
-    //create a polygon object
-    public Polygon u_ship=new Polygon(-10,-10,20,0,-10,10);
-    
-}

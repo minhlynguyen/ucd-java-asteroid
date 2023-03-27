b4f4259a8052a6eb702ce301bb3d62b40ddb1420
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-03-27 10:31:25 +0100

    Update AlienShipMovementAndShooting.java

--- demo/src/main/java/asteroid_app/initial/AlienShipMovementAndShooting.java
+++ demo/src/main/java/asteroid_app/initial/AlienShipMovementAndShooting.java
@@ -1,34 +1,47 @@
 package asteroid_app.initial;
 
-// A class that represents the movement and shooting behavior of an alien ship in the game.
+import java.util.Random;
+
 public class AlienShipMovementAndShooting {
     private double velocityX;
     private double velocityY;
     private long lastShotTime;
+    private long lastVelocityUpdateTime;
     private long shotInterval = 1000; // Interval between shots in milliseconds
+    private long velocityUpdateInterval = 2000; // Interval between velocity updates in milliseconds
+    private Random random = new Random();
 
-    // Constructor that initializes the AlienShipMovementAndShooting object with the given velocities.
     public AlienShipMovementAndShooting(double velocityX, double velocityY) {
         this.velocityX = velocityX;
         this.velocityY = velocityY;
     }
 
-    // Method that updates the position and shooting behavior of the given Alien object.
-    public void update(Alien alien, long now) {
-        // Update the Alien object's position based on its velocities
+    public void update(Alien alien, Player player, long now) {
         alien.setTranslateX(alien.getTranslateX() + velocityX);
         alien.setTranslateY(alien.getTranslateY() + velocityY);
 
-        // Check if it's time for the Alien object to shoot
         if (now - lastShotTime > shotInterval * 1000000) { // Convert to nanoseconds
-            shoot(alien);
+            shoot(alien, player);
             lastShotTime = now;
+        }
+
+        if (now - lastVelocityUpdateTime > velocityUpdateInterval * 1000000) { // Convert to nanoseconds
+            updateVelocity();
+            lastVelocityUpdateTime = now;
         }
     }
 
-    // Method that handles shooting behavior for the given Alien object.
-    public void shoot(Alien alien) {
-        // Create bullet object and set its trajectory
-        // The implementation details are missing and need to be added.
+    public void shoot(Alien alien, Player player) {
+        // Create bullet object and set its trajectory towards the player's current position
+        // The implementation are missing 
+    }
+
+    private void updateVelocity() {
+        // Update velocities randomly
+        double angle = random.nextDouble() * 2 * Math.PI;
+        double speed = 2; 
+
+        velocityX = Math.cos(angle) * speed;
+        velocityY = Math.sin(angle) * speed;
     }
 }

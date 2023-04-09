99d57e814147ce38c6af0b3045b292c450500034
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-09 17:37:16 +0100

    Update AlienShipCreation.java

--- demo/src/main/java/asteroid_app/initial/AlienShipCreation.java
+++ demo/src/main/java/asteroid_app/initial/AlienShipCreation.java
@@ -3,12 +3,14 @@ package asteroid_app.initial;
 // A class that represents the creation of an alien ship in the game.
 public class AlienShipCreation {
 
+    // Constructor that takes the x and y coordinates as parameters.
+    public AlienShipCreation() {
+    }
+
     // Method that creates a new Alien object 
     // with the given x and y coordinates.
     public Alien createAlienShip(double x, double y) {
-        Alien alien = new Alien(); // Create a new Alien object
-        alien.setTranslateX(x); // Set the x coordinate
-        alien.setTranslateY(y); // Set the y coordinate
+        Alien alien = new Alien(x, y); // Create a new Alien object
         return alien; // Return the created Alien object
     }
 }

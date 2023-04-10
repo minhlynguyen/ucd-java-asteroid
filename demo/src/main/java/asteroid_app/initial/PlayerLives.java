dae1d48fe669c28c43e5013c010b0001433f5e92
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-04-10 14:41:42 +0100

    Change package to look like main

--- demo/src/main/java/asteroid_app/initial/PlayerLives.java
+++ demo/src/main/java/asteroid_app/initial/PlayerLives.java
@@ -1,5 +1,5 @@
-package asteroid_app.initial;
-// package.demo.src.main.java.asteroid_app.initial
+// package asteroid_app.initial;
+package.demo.src.main.java.asteroid_app.initial
 
 // A class that represents the lives of a player in a game.
 public class PlayerLives {

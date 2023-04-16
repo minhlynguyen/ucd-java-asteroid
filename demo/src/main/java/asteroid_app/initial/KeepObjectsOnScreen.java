6a8cc6c00a830f5f6ef7c61b4285606eb591288c
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 00:26:24 +0100

    Update KeepObjectsOnScreen.java

--- demo/src/main/java/asteroid_app/initial/KeepObjectsOnScreen.java
+++ demo/src/main/java/asteroid_app/initial/KeepObjectsOnScreen.java
@@ -1,5 +1,6 @@
 package asteroid_app.initial;
 
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 
 public class KeepObjectsOnScreen {
@@ -7,37 +8,39 @@ public class KeepObjectsOnScreen {
     private int screenHeight;
     private boolean isOffScreen = false;
 
-    public KeepObjectsOnScreen(int screenWidth, int screenHeight) {
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
+   public KeepObjectsOnScreen(int screenWidth, int screenHeight) {
+    this.screenWidth = screenWidth;
+    this.screenHeight = screenHeight;
+}
+
+public void checkBoundaries(Node node) {
+    double x = node.getTranslateX();
+    double y = node.getTranslateY();
+    double radius = Math.max(node.getBoundsInLocal().getWidth(), node.getBoundsInLocal().getHeight()) / 2;
+
+    // Check if the node is off the left or right edge of the screen
+    if (x < -radius) {
+        x = screenWidth + radius;
+        isOffScreen = true;
+    } else if (x > screenWidth + radius) {
+        x = -radius;
+        isOffScreen = true;
     }
 
-    public void checkBoundaries(Ship ship) {
-        int x = ship.getPosition().x;
-        int y = ship.getPosition().y;
-        int radius = ship.getRadius();
-
-        // Check if the ship is off the left or right edge of the screen
-        if (x < -radius) {
-            x = screenWidth + radius;
-            isOffScreen = true;
-        } else if (x > screenWidth + radius) {
-            x = -radius;
-            isOffScreen = true;
-        }
-
-        // Check if the ship is off the top or bottom edge of the screen
-        if (y < -radius) {
-            y = screenHeight + radius;
-            isOffScreen = true;
-        } else if (y > screenHeight + radius) {
-            y = -radius;
-            isOffScreen = true;
-        }
-
-        if (isOffScreen) {
-            ship.setPosition(new Point(x, y));
-            isOffScreen = false;
-        }
+    // Check if the node is off the top or bottom edge of the screen
+    if (y < -radius) {
+        y = screenHeight + radius;
+        isOffScreen = true;
+    } else if (y > screenHeight + radius) {
+        y = -radius;
+        isOffScreen = true;
     }
+
+    if (isOffScreen) {
+        node.setTranslateX(x);
+        node.setTranslateY(y);
+        isOffScreen = false;
+    }
+  }
+
 }

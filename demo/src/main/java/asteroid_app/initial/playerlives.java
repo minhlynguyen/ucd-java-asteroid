c64c4b5e44cbcc77cef01b994bf7895d05326362
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 00:29:42 +0100

    Update playerlives.java

--- demo/src/main/java/asteroid_app/initial/playerlives.java
+++ demo/src/main/java/asteroid_app/initial/playerlives.java
@@ -1,40 +1,54 @@
-package asteroid_app.initial;
+package asteroid_app.modified;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
-// A class that represents the lives of a player in a game.
-public class PlayerLives {
-   private IntegerProperty lives;
+// A class to keep track of the player's score.
+public class IncrementScore {
+    public static final int POINTS_FOR_ASTEROID = 10;
+    public static final int POINTS_FOR_ALIEN = 50;
+    public static final int POINTS_FOR_EXTRA_LIFE = 10000;
 
-    // Constructor that initializes the PlayerLives object with 3 lives.
-public PlayerLives() {
-    this.lives = new SimpleIntegerProperty(3); // Default value is 3
-}
+    private IntegerProperty score = new SimpleIntegerProperty(0);
+    private int lives = 3;
 
-// Method that decrements the number of lives by 1.
-// Returns true if the player still has lives left, false otherwise.
-public boolean loseLife() {
-    int currentLives = lives.get();
-    currentLives--;
-    lives.set(currentLives);
-    return currentLives > 0;
-}
+    // Constructor that initializes the score to zero.
+    public IncrementScore() {
+        score.set(0);
+    }
 
-// Method that increments the number of lives by 1.
-public void gainLife() {
-    int currentLives = lives.get();
-    currentLives++;
-    lives.set(currentLives);
-}
+    // Method that returns the current score.
+    public int getScore() {
+        return score.get();
+    }
 
-// Method that returns the current number of lives.
-public IntegerProperty livesProperty() {
-    return lives;
-}
+    // Method that returns the current number of lives.
+    public int getLives() {
+        return lives;
+    }
 
-public int getLives() {
-    return lives.get();
-   }
+    // Method that increments the score by the specified amount.
+    public void incrementScore(int points) {
+        score.set(score.get() + points);
+        if (score.get() >= POINTS_FOR_EXTRA_LIFE) {
+            int extraLives = score.get() / POINTS_FOR_EXTRA_LIFE;
+            lives += extraLives;
+            score.set(score.get() % POINTS_FOR_EXTRA_LIFE);
+        }
+    }
+
+    // Method that increments the score by the standard amount for an asteroid.
+    public void incrementScoreForAsteroid() {
+        incrementScore(POINTS_FOR_ASTEROID);
+    }
+
+    // Method that increments the score by the standard amount for an alien.
+    public void incrementScoreForAlien() {
+        incrementScore(POINTS_FOR_ALIEN);
+    }
+
+    // JavaFX property accessor for score.
+    public IntegerProperty scoreProperty() {
+        return score;
+    }
 }
-    

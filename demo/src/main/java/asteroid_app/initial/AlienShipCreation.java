17654e477b26738639b69421e84e5b8c64a45099
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 14:10:10 +0100

    Update AlienShipCreation.java

--- demo/src/main/java/asteroid_app/initial/AlienShipCreation.java
+++ demo/src/main/java/asteroid_app/initial/AlienShipCreation.java
@@ -1,8 +1,11 @@
 package asteroid_app.initial;
 
+
+import asteroid_app.custom.AlienShip;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
 
 public class AlienShipCreation {
     private int screenWidth;
@@ -17,17 +20,34 @@ public class AlienShipCreation {
 
     private void initAlienShip() {
         alienShip = new AlienShip(0, 0);
-        alienShip.setChar(Alien.createAlien());
     }
 
     public AlienShip getAlienShip() {
         return alienShip;
     }
 
-    public void drawAlienShip(GraphicsContext graphics) {
+     public void drawAlienShip(GraphicsContext graphics) {
         if (alienShip.isActive()) {
             graphics.setFill(Color.BLACK);
-            graphics.fillPolygon(alienShip.getChar().getPoints().stream().toArray(Double[]::new), alienShip.getChar().getPoints().stream().toArray(Double[]::new), alienShip.getChar().getPoints().size());
+            Polygon shape = alienShip.getShape();
+            Point2D position = alienShip.getPosition();
+
+            // Save the current graphics transform
+            graphics.save();
+
+            // Set the rotation and translation for the alien ship
+            Rotate rotation = new Rotate(0, position.getX(), position.getY());
+            graphics.setTransform(rotation.getMxx(), rotation.getMyx(), rotation.getMxy(), rotation.getMyy(), rotation.getTx(), rotation.getTy());
+
+            // Draw the alien ship
+            graphics.fillPolygon(
+                    shape.getPoints().stream().mapToDouble(Double::doubleValue).toArray(),
+                    shape.getPoints().stream().mapToDouble(Double::doubleValue).toArray(),
+                    shape.getPoints().size() / 2
+            );
+
+            // Restore the original graphics transform
+            graphics.restore();
         }
     }
 }

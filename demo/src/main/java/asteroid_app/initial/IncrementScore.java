59bd77d5e46e8d3c438ef6a079109bf3e47cd441
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 10:51:14 +0100

    Update IncrementScore.java

--- demo/src/main/java/asteroid_app/initial/IncrementScore.java
+++ demo/src/main/java/asteroid_app/initial/IncrementScore.java
@@ -1,25 +1,40 @@
 package asteroid_app.initial;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 // A class to keep track of the player's score.
 public class IncrementScore {
     public static final int POINTS_FOR_ASTEROID = 10;
     public static final int POINTS_FOR_ALIEN = 50;
+    public static final int POINTS_FOR_EXTRA_LIFE = 10000;
 
-    private int score;
+    private IntegerProperty score = new SimpleIntegerProperty(0);
+    private int lives = 3;
 
     // Constructor that initializes the score to zero.
     public IncrementScore() {
-        score = 0;
+        score.set(0);
     }
 
     // Method that returns the current score.
     public int getScore() {
-        return score;
+        return score.get();
+    }
+
+    // Method that returns the current number of lives.
+    public int getLives() {
+        return lives;
     }
 
     // Method that increments the score by the specified amount.
     public void incrementScore(int points) {
-        score += points;
+        score.set(score.get() + points);
+        if (score.get() >= POINTS_FOR_EXTRA_LIFE) {
+            int extraLives = score.get() / POINTS_FOR_EXTRA_LIFE;
+            lives += extraLives;
+            score.set(score.get() % POINTS_FOR_EXTRA_LIFE);
+        }
     }
 
     // Method that increments the score by the standard amount for an asteroid.
@@ -30,5 +45,10 @@ public class IncrementScore {
     // Method that increments the score by the standard amount for an alien.
     public void incrementScoreForAlien() {
         incrementScore(POINTS_FOR_ALIEN);
+    }
+
+    // JavaFX property accessor for score.
+    public IntegerProperty scoreProperty() {
+        return score;
     }
 }

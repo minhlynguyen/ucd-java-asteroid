fbd6458915b2cebc22e932c1f25d6c13a45110ea
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 23:40:38 +0100

    Update Asteroid.java

--- demo/src/main/java/asteroid_app/initial/Asteroid.java
+++ demo/src/main/java/asteroid_app/initial/Asteroid.java
@@ -1,44 +1,101 @@
 package asteroid_app.initial;
 
 import java.util.Random;
+import java.util.List;
+import javafx.scene.layout.Pane;
 
-import javafx.scene.shape.Polygon;
+public class Asteroid extends Character {
 
-public class Asteroid extends Character{
-	
-//	List<Asteroid> asteroids = new ArrayList<>();
-	
-	public static Polygon createAsteroid(){
-		
+	private double movingAngle;
+	private int initialsize;
+
+	// Except variables x,y inherited from character, asteroid also needs to set its
+	// size
+	public Asteroid(double x, double y, int size) {
+
+		// Use PolygonCreator class to create an asteroid polygon
+		super(new PolygonCreator().createPolygon(size), x, y);
+
+		// Only asteroid needs to set an initial angle, which can use initialAngle to
+		// express
+		// Because the direction is random, so we use Random() to generate an angle
 		Random rnd = new Random();
-		//side is the length of polygon's one side
-		double side = 50+rnd.nextInt(10);
-		Polygon asteroid = new Polygon();
-		
-		double c1 = Math.cos(Math.PI/3);
-		double s1 = Math.sin(Math.PI/3);
-		double c2 = Math.cos(Math.PI/6);
-		double s2 = Math.sin(Math.PI/6);
-		
-		asteroid.getPoints().addAll(
-				0.0, 0.0,
-				side*c1, -1*side*s1,
-				side*c2, -1*side*s2,
-				side*c2, side*s2,
-				side*c1, side*s1,
-				-1*side*c1, side*s1,
-				-1*side*c2, -1*side*s2,
-				-1*side*c1, -1*side*s1
-				);
-		
 
-		initX =400;
-		initY = 400;
-		asteroid.setTranslateX(initX);
-		asteroid.setTranslateY(initY);
-		
-		System.out.println(asteroid);
-		return asteroid;			
+		// The max angle is 360
+		// We import setRotate method to set its initial angel
+		super.getChar().setRotate(rnd.nextDouble() * 360);
+
+		// Generate a fixed moving angle
+		this.movingAngle = 0.5 - rnd.nextDouble();
+
+		this.initialsize = size;
+
+		// increase their initial speed by accelerating upon creation
+		// increase this as levels increase?
+		for (int i = 0; i < rnd.nextInt(150); i++) {
+			if (size == 3) {
+				this.accelerate(0.005);
+			} else if (size == 2) {
+				this.accelerate(0.01);
+			} else {
+				this.accelerate(0.5);
+			}
+		}
 	}
-	
+
+	// Asteroid has a random moving direction, so we need to set a override part in
+	// move() method
+	@Override
+	public void move() {
+		super.move();
+		super.getChar().setRotate(super.getChar().getRotate() + movingAngle);
+	}
+
+	public int getInitialSize() {
+		return initialsize;
+	}
+
+	// function of asteroids split
+	public static void asteroidSplit(Asteroid asteroid, List<Asteroid> asteroids, Pane pane) {
+        // Then add two new smaller asteroids on the scene and in the asteroids list
+        // If the size=3 asteroid is collided
+        if (asteroid.getInitialSize() == 3) {
+            // add new size 2 asteroids
+            for (int i = 0; i < 2; i++) {
+                Asteroid newAsteroid = new Asteroid(
+                        asteroid.getChar().getTranslateX(),
+                        asteroid.getChar().getTranslateY(), 2);
+                asteroids.add(newAsteroid);
+                pane.getChildren().add(newAsteroid.getChar());
+                newAsteroid.move();
+                // remove the collided asteroid
+                pane.getChildren().remove(asteroid.getChar());
+                asteroids.remove(asteroid);
+            }
+        }
+
+        // If the size=2 asteroid is collided:
+        else if (asteroid.getInitialSize() == 2) {
+            for (int i = 0; i < 2; i++) {
+                // add new size 1 asteroids
+                Asteroid newAsteroid = new Asteroid(
+                        asteroid.getChar().getTranslateX(),
+                        asteroid.getChar().getTranslateY(), 1);
+                asteroids.add(newAsteroid);
+                pane.getChildren().add(newAsteroid.getChar());
+                newAsteroid.move();
+                // remove the collided asteroid
+                pane.getChildren().remove(asteroid.getChar());
+                asteroids.remove(asteroid);
+            }
+        }
+
+        // if the size=1 asteroid is collided
+        else {
+            // remove the collided asteroid
+            pane.getChildren().remove(asteroid.getChar());
+            asteroids.remove(asteroid);
+        }
+    }
+
 }

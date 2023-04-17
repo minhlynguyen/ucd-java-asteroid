d406a3b847a39f755685ccdf83187b808c0d5716
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 10:34:58 +0100

    Update GameOverMenu.java

--- demo/src/main/java/asteroid_app/initial/GameOverMenu.java
+++ demo/src/main/java/asteroid_app/initial/GameOverMenu.java
@@ -12,36 +12,27 @@ import javafx.scene.text.FontWeight;
 import static javafx.scene.text.FontWeight.*;
 
 public class GameOverMenu {
-    public static Scene gameOverMenu(int WIDTH, int HEIGHT){
-        // ------ GAME OVER SCENE ------
-        // GridPane gridPane = new GridPane();
-        // gridPane.setAlignment(Pos.BASELINE-CENTER);
-        // gridPane.setHgap(10);
-        // gridPane.setVgap(30);
-        // gridPane.setPadding(new Insets(0, 10, 0, 10));
-
-        // Label nameField = new Label("Enter your name:");
-        TextField nameText = new TextField();
-        nameText.setPrefWidth(100);
-        // gridPane.add(nameField, 0, 0);
-        // gridPane.add(nameText, 1, 0);
+    public static Scene gameOverMenu(int WIDTH, int HEIGHT, IncrementScore score, PlayerLives lives){
+        int topTenMin = 4000;
+        int finalScore = score.getScore() + lives.getLives() * 1000;
 
         Label headlineover = new Label("GAME OVER");
         headlineover.setFont(Font.font("Monospaced", FontWeight.BOLD, 50));
-        // Button playGameSm = new Button("PLAY GAME");
+
         Label yourScore = new Label("Your score is");
 
-        // Score update later
-        int topTenMin = 4000;
-        int finalScore = 5000;
-
-        Label score = new Label(""+finalScore);
+        Label scoreLabel = new Label("" + finalScore);
+        Label livesLabel = new Label("Lives left: " + lives.getLives());
         Label yourName = new Label("Enter your name");
+
+        TextField nameText = new TextField();
+        nameText.setPrefWidth(100);
 
         Button saveScore = new Button("Save");
         saveScore.setId("saveScore");
+
         Button seeHighScore = new Button("High Scores");
-        saveScore.setId("seeHighScore");
+        seeHighScore.setId("seeHighScore");
 
         VBox infoBox;
 

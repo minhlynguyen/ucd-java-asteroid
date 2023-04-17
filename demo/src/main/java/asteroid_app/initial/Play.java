4aeb324955956181240187051cb0368a9ba5966c
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 14:51:24 +0100

    Update Play.java

--- demo/src/main/java/asteroid_app/initial/Play.java
+++ demo/src/main/java/asteroid_app/initial/Play.java
@@ -1,5 +1,19 @@
 package asteroid_app.initial;
 
 public class Player {
+    private PlayerLives playerLives;
+    private IncrementScore incrementScore;
 
+    public Player() {
+        playerLives = new PlayerLives();
+        incrementScore = new IncrementScore();
+    }
+
+    public PlayerLives getPlayerLives() {
+        return playerLives;
+    }
+
+    public IncrementScore getIncrementScore() {
+        return incrementScore;
+    }
 }

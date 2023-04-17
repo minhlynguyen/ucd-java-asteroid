1fd2d762bd81592d74c459f7e201905b64049839
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-17 14:19:11 +0100

    Update user_ship.java

--- demo/src/main/java/asteroid_app/initial/user_ship.java
+++ demo/src/main/java/asteroid_app/initial/user_ship.java
@@ -1,9 +1,22 @@
 package asteroid_app.initial;
 //import polygon to draw a polygon
+import javafx.geometry.Point2D;
 import javafx.scene.shape.Polygon;
 
-public class user_ship{
-    //create a polygon object
-    public Polygon u_ship=new Polygon(-10,-10,20,0,-10,10);
+private Polygon u_ship;
+    private Point2D position;
+
+    public user_ship(double x, double y) {
+        u_ship = new Polygon(-10, -10, 20, 0, -10, 10);
+        position = new Point2D(x, y);
+    }
+
+    public Polygon getU_ship() {
+        return u_ship;
+    }
+
+    public Point2D getPosition() {
+        return position;
+    }
     
 }

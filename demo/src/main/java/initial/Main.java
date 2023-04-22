5a49546554d9963932efb4c7d0c96a2ec5544f77
ShanieWang <123460651+ShanieWang@users.noreply.github.com>
2023-04-22 10:10:34 +0100

    Revert "Delete Main.java"
    This reverts commit 3511f9d46108a0bcebdb8a7f376894442aaa92d3.

---
+++ demo/src/main/java/initial/Main.java
@@ -0,0 +1,67 @@
+package asteroid_app.initial;//this is the package name
+// package demo.src.main.java.asteroid_app.initial;
+
+// Application is the base class for all JavaFX applications
+//need javafx to display the window
+import javafx.application.Application;
+// Stage is the top level JavaFX container
+import javafx.stage.Stage;
+// Scene is the container for all content
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+
+//window extends the application class from javafx
+public class Main extends Application {
+
+    public static Stage stage;
+
+    // define the size of the screen can be accessed by all classes
+    public static int WIDTH = 1400;
+    public static int HEIGHT = 800;
+    // the window class overrides the start mehtod from the application class
+    // takes a single parameter of type stage
+    // inside the start method is where the User interface is created
+    public static int pointX = WIDTH / 2 - 7;
+    public static int pointY = 20;
+
+    // initial level = 1
+    public static int initLevel = 1;
+
+    @Override
+    public void start(Stage stage) {
+
+        Main.stage = stage;
+
+        // start Menu
+        Scene startMenuScene = StartMenu.startMenu(WIDTH, HEIGHT);
+        // game over menu
+        Scene gameOverScene = GameOverMenu.gameOverMenu(WIDTH, HEIGHT);
+        // game menu
+        Scene mainScene = GameMenu.newGameMenu(initLevel,stage,gameOverScene);
+        // default the start menu 
+        stage.setScene(startMenuScene);
+
+        // get the play game button
+        Button playGameButton = (Button) startMenuScene.getRoot().lookup("#playGame");
+        // click the play game button then change to game menu
+        playGameButton.setOnAction(e -> stage.setScene(mainScene));
+
+        // When click on quit button, enter the gameover scene
+        Button quitGameButton = (Button) mainScene.getRoot().lookup("#quitGame");
+        quitGameButton.setOnAction(e -> stage.setScene(gameOverScene));
+
+        Button restartGameButton = (Button) mainScene.getRoot().lookup("#restartGame");
+        restartGameButton.setOnAction(e -> stage.setScene(startMenuScene));
+
+        // set the title of the window
+        stage.setTitle("Group 11-Asteroids Game");
+        // display the stage
+        stage.show();
+    }
+
+    // run the application
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

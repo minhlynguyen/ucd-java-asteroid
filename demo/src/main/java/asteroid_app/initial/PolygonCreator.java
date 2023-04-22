abeb9e4242ca156f9d8708dcc15ca6598ab9c90e
kylinwang318 <96105366+kylinwang318@users.noreply.github.com>
2023-04-22 11:09:12 +0100

    Update PolygonCreator.java

--- demo/src/main/java/asteroid_app/initial/PolygonCreator.java
+++ demo/src/main/java/asteroid_app/initial/PolygonCreator.java
@@ -5,7 +5,7 @@ import javafx.scene.shape.Polygon;
 
 public class PolygonCreator {
 
-	public Polygon createPolygon(int size) {
+	public Polygon createPolygon(Size size) {
 
 		Polygon polygon = new Polygon();
 
@@ -15,19 +15,51 @@ public class PolygonCreator {
 		double s2 = Math.sin(Math.PI / 6);
 
 		Random rnd = new Random();
-		// side is the length of polygon's one side
-		double side = (20 + rnd.nextInt(10)) * size;
 
-		polygon.getPoints().addAll(
-				0.0, 0.0,
-				side * c1, -1 * side * s1,
-				side * c2, -1 * side * s2,
-				side * c2, side * s2,
-				side * c1, side * s1,
-				-1 * side * c1, side * s1,
-				-1 * side * c2, -1 * side * s2,
-				-1 * side * c1, -1 * side * s1);
+		switch (size) {
+			case LARGE:{
+				// side is the length of polygon's one side
+				int side = (20 + rnd.nextInt(10)) * 3;
+				polygon.getPoints().addAll(
+						0.0, 0.0,
+						side * c1, -1 * side * s1,
+						side * c2, -1 * side * s2,
+						side * c2, side * s2,
+						side * c1, side * s1,
+						-1 * side * c1, side * s1,
+						-1 * side * c2, -1 * side * s2,
+						-1 * side * c1, -1 * side * s1);
+			}
+				break;
+			case MIDDLE: {
+				int side = (20 + rnd.nextInt(10)) * 2;
+				polygon.getPoints().addAll(
+						0.0, 0.0,
+						side * c1, -1 * side * s1,
+						side * c2, -1 * side * s2,
+						side * c2, side * s2,
+						side * c1, side * s1,
+						-1 * side * c1, side * s1,
+						-1 * side * c2, -1 * side * s2,
+						-1 * side * c1, -1 * side * s1);
+			}
+				break;
+			case SMALL:{
+				int side = (20 + rnd.nextInt(10));
+				polygon.getPoints().addAll(
+						0.0, 0.0,
+						side * c1, -1 * side * s1,
+						side * c2, -1 * side * s2,
+						side * c2, side * s2,
+						side * c1, side * s1,
+						-1 * side * c1, side * s1,
+						-1 * side * c2, -1 * side * s2,
+						-1 * side * c1, -1 * side * s1);
+			}
+				break;
+		}
 
 		return polygon;
+
 	}
 }

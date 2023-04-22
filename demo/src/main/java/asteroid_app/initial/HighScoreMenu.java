afcb31ec7de2e8b88b4ab35de1d08dc78bc45205
iquranaseem <123598068+iquranaseem@users.noreply.github.com>
2023-04-22 19:54:42 +0100

    Update HighScoreMenu.java

--- demo/src/main/java/asteroid_app/initial/HighScoreMenu.java
+++ demo/src/main/java/asteroid_app/initial/HighScoreMenu.java
@@ -1,8 +1,12 @@
 package asteroid_app.initial;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public class HighScoreMenu {
@@ -16,6 +20,22 @@ public class HighScoreMenu {
         // create a Vbox to manage the nodes on the high score menu
         VBox vBox = new VBox(20, headLine);
         vBox.setId("highScoreVbox");
+
+        // read high scores from file
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("highscores.txt"));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Label score = new Label(line);
+                vBox.getChildren().add(score);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            Label noScores = new Label("There are no high scores.");
+            vBox.getChildren().add(noScores);
+        }
 
         // add vBox into root
         borderPane.setCenter(vBox);
